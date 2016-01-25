834fbd6e3388c5c617442954f2411f07c35264df
Damien Metzler <dmetzler@nuxeo.com>
2016-01-25 11:51:20 +0100

    Dynamically add servelt to Jetty Harness

--- src/main/java/org/isen/jee/exam/bowling/BowlingScoreServlet.java
+++ src/main/java/org/isen/jee/exam/bowling/BowlingScoreServlet.java
@@ -1,11 +1,14 @@
 package org.isen.jee.exam.bowling;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
+@WebServlet("/score")
 public class BowlingScoreServlet extends HttpServlet {
 
     @Override
